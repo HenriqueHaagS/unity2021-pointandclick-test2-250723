2a84e52d088444187be32a220c1d87310023aecd
HenriqueHaagS <138019535+HenriqueHaagS@users.noreply.github.com>
2023-08-03 14:14:39 -0300

    NAVmesh working, Animation Blend Tree working

---
+++ unity2021-pointandclick-test2-250723/Assets/Scripts/CharController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharController : MonoBehaviour
+{
+    private Vector2 followSpot;
+    public float speed;
+    public float perspectiveScale;
+    public float scaleRatio;
+
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    private Vector2 stuckDistanceCheck;
+
+    void Start()
+    {
+        followSpot = transform.position;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        animator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            followSpot = new Vector2(mousePosition.x, mousePosition.y);
+        }
+        agent.SetDestination(new Vector3(followSpot.x, followSpot.y, transform.position.z));
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        float distance = Vector2.Distance(transform.position, followSpot);
+        if (Vector2.Distance(stuckDistanceCheck, transform.position) == 0)
+        {
+            animator.SetFloat("Distance", 0f);
+            return;
+        }
+        animator.SetFloat("Distance", distance);
+
+        if (distance > 0.01)
+        {
+            Vector3 direction = transform.position - new Vector3(followSpot.x, followSpot.y, transform.position.z);
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            animator.SetFloat("Angle", angle);
+            stuckDistanceCheck = transform.position;
+        }
+        
+    }
+}
